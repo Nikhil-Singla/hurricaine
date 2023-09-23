f07ac0cb180c448175f9eb1b6d63ec052a28e15d
Nikhil Singla <nsingla3.14@gmail.com>
2023-09-23 16:19:45 -0500

    Implemented Floating Dropdown

--- hurricaine/src/Components/Sidebar.js
+++ hurricaine/src/Components/Sidebar.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 
 const Sidebar = () => {
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col" class="dropdown-content">
       <p className="w-full">Local Weather Information</p>
       <p className="w-full">Area Emergency Services</p>
       <p className="w-full">Emergency Preparedness</p>
