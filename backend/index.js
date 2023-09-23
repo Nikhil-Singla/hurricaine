09e41046681709d6283a49c2d2788f8cd07c2049
mohdm2611 <mohdm2611@gmail.com>
2023-09-23 16:51:06 -0500

    Created routes for weather

--- backend/index.js
+++ backend/index.js
@@ -3,8 +3,6 @@ require("dotenv").config();
 const { Configuration, OpenAI } = require('openai');
 const axios = require('axios');
 const express = require('express')
-const weatherRoute = require('./routes/weather');
-const askgptRoute = require('./routes/askgpt');
 
 const app = express();
 const PORT = process.env.PORT || 4000;
@@ -24,7 +22,8 @@ app.get('/', (req, res) => {
   res.send('Hello there!')
 })
 
-// app.get('/askgpt', async (req, res) => {
+app.get('/askgpt', async (req, res) => {
+    res.send('GPT Endpoint')
     // const chatCompletion = await openai.chat.completions.create({
     //     model: "gpt-3.5-turbo",
     //     messages: [{"role": "user", "content": "Hello!"}],
@@ -34,11 +33,12 @@ app.get('/', (req, res) => {
     //   success: true,
     //   message: chatCompletion.choices[0].message,
     // });
-// });
+});
 
+app.get('/weather', async (req, res) => {
+    res.send('Weather Endpoint');
+});
 
-app.use('/weather', weatherRoute);
-app.use('/askgpt', askgptRoute);
 
  
 
--- backend/routes/weather.js
+++
@@ -1,7 +0,0 @@
-const express=require('express')
-const router=express.Router()
-router.get("/",(req,res)=>{
-    res.send("Weather data")
-})
-
-module.exports=router;
