88f698772e135223577021784de4b1b8a7b601d5
mohdm2611 <mohdm2611@gmail.com>
2023-09-23 16:33:16 -0500

    Created routes for weather

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,19 @@
 // index.js
+require("dotenv").config();
+const { Configuration, OpenAI } = require('openai');
+const axios = require('axios');
 const express = require('express')
+const weatherRoute = require('./routes/weather');
 
-const app = express()
-const PORT = 4000
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+
+const openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY,
+});
+
+
 
 app.listen(PORT, () => {
   console.log(`API listening on PORT ${PORT} `)
@@ -12,10 +23,23 @@ app.get('/', (req, res) => {
   res.send('Hello there!')
 })
 
-app.get('/askgpt', (req, res) => {
-    console.log(req)
-    res.send(req.query.query)
-})
+app.get('/askgpt', async (req, res) => {
+    // const chatCompletion = await openai.chat.completions.create({
+    //     model: "gpt-3.5-turbo",
+    //     messages: [{"role": "user", "content": "Hello!"}],
+    //   });
+    // console.log(chatCompletion);
+    // return res.status(200).json({
+    //   success: true,
+    //   message: chatCompletion.choices[0].message,
+    // });
+});
+
+
+app.use('/weather', weatherRoute)
+
+ 
+
 
 // Export the Express API
 module.exports = app
