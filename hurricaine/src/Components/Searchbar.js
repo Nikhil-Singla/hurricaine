a379edeb460f77f21314b64d2587dc2a1db13976
Lulu Cao <lcao@dimin.com>
2023-09-23 14:57:46 -0500

    set up basics for searchbar

--- hurricaine/src/Components/Searchbar.js
+++ hurricaine/src/Components/Searchbar.js
@@ -1,11 +1,40 @@
 import React, { useState } from "react";
+import axios from 'axios';
 
-const Sidebar = () => {
+const Searchbar = () => {
+  const [input, setInput] = useState("");
+  const search = () => {
+    console.log(input);
+    setInput(input);
+    console.log(input);
+    axios.get(`https://hackwidwest-backend.vercel.app/askgpt?query=${input}`)
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      })
+      .finally(function () {
+      });
+  };
+
   return (
-    <div className="flex justify-center mt-40">
-      <p className="w-1/2 h-12 border-2 border-gray-900 rounded"></p>
+    <div className="flex justify-center align-middle mt-40">
+      <div className="w-1/2 h-12 border-2 border-gray-900 rounded flex justify-end">
+        <input className="w-full px-2" onInput={e=>setInput(e.target.value)} />
+        <div className="flex align-middle justify-center w-8 h-12">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M12 18.75a6 6 0 006-6v-1.5m-6 7.5a6 6 0 01-6-6v-1.5m6 7.5v3.75m-3.75 0h7.5M12 15.75a3 3 0 01-3-3V4.5a3 3 0 116 0v8.25a3 3 0 01-3 3z" />
+          </svg>
+        </div>
+        <div className="w-8 h-8" onClick={search}>
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
+          </svg>
+        </div>
+      </div>
     </div>
   )
 }
 
-export default Sidebar;
+export default Searchbar;
