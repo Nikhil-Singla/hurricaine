8d4c6d997481dc5703e31f96331af8cabce6b7d8
Nikhil Singla <nsingla3.14@gmail.com>
2023-09-23 20:01:28 -0500

    Added User images, and appending icons

--- hurricaine/src/Components/Nav.js
+++ hurricaine/src/Components/Nav.js
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
+import './Nav.css';
+
 
 const Nav = () => {
   return (
     <div>
-      <h1 className="text-3xl my-20">HurricAIne</h1>
+      <h1 className="text-3xl my-20">Hurric<span className="Loop">AI</span>ne</h1>
     </div>
   )
 }
