05847bd7e62b3176b9b2e544154ec075ca53bf01
Nikhil Singla <nsingla3.14@gmail.com>
2023-09-23 19:14:10 -0500

    Logo, Animated name

--- hurricaine/src/Components/SidebarMain.js
+++ hurricaine/src/Components/SidebarMain.js
@@ -2,16 +2,19 @@ import React from 'react';
 import { slide as Menu } from 'react-burger-menu';
 import './SidebarMain.css';
 import Knowledge from './Knowledge';
+import img from '../assets/Tornado.png';
 
 export default props => {
   return (
     <Menu>
-      <a className="menu-item" href="/">
+      <a href="/" className="paddingBottom">
         Home
       </a>
-      <a className="menu-item" href="/knowledge">
+      <p className="paddingBottom">
         <Knowledge />
-      </a>
+      </p>
+      <img src={img} className='logoCopy' alt="Logo"></img> 
+      <p className='logoText'>Hurric<span className="Loop">AI</span>ne</p>
     </Menu>
   );
 };
