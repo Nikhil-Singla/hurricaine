0de8eeff9fe3216d28ae21789211a5adbf6e88c7
Nikhil Singla <nsingla3.14@gmail.com>
2023-09-23 23:53:53 -0500

    Linking stuff

--- hurricaine/src/Components/Sidebar.js
+++ hurricaine/src/Components/Sidebar.js
@@ -3,10 +3,10 @@ import React, { useState } from "react";
 const Sidebar = () => {
   return (
     <div className="flex flex-col dropdown-content">
-      <a href="https://www.ready.gov/faq" className="w-full">Local Weather Information</a>
+      <a href="https://openweathermap.org/" className="w-full">Local Weather Information</a>
       <a href="??" className="w-full">Area Emergency Services</a>
-      <a href="??" className="w-full">Emergency Preparedness</a>
-      <a href="??" className="w-full">Frequently Asked Questions</a>
+      <a href="https://www.fema.gov/pdf/areyouready/basic_preparedness.pdf" className="w-full">Emergency Preparedness</a>
+      <a href="https://www.ready.gov/faq" className="w-full">Frequently Asked Questions</a>
     </div>
   )
 }
