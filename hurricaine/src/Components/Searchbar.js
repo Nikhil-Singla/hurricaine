46f85df387e48caeb7e5351224972a6a7e85f0a9
Lulu Cao <lcao@dimin.com>
2023-09-24 08:39:41 -0500

    removed map and x scroll

--- hurricaine/src/Components/Searchbar.js
+++ hurricaine/src/Components/Searchbar.js
@@ -162,7 +162,7 @@ const Searchbar = () => {
 
   return (
     <div className="convo-box">
-      <div className="overflow-scroll search-result-container" id="Scroller">
+      <div className="overflow-y-scroll search-result-container" id="Scroller">
         <div className="flex w-full justify-center">
           <div className="w-1/2 text-start" id="searchResult" >
           </div>
@@ -196,12 +196,12 @@ const Searchbar = () => {
               </svg>
             </div>
           }
-          <div className="searchElement -mr-2" onClick={()=>showMap(!mapOpen)}>
+          {/* <div className="searchElement -mr-2" onClick={()=>showMap(!mapOpen)}>
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-7">
               <path strokeLinecap="round" strokeLinejoin="round" d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z" />
               <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z" />
             </svg>
-          </div>
+          </div> */}
           <div className="searchElement" onClick={handleSearch}>
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
               <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
