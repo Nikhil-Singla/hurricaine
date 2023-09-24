f7ea2dceb83bd515418e35db8e0a3f5821552218
Lulu Cao <lcao@dimin.com>
2023-09-24 07:42:04 -0500

    added map button

--- hurricaine/src/Components/Searchbar.js
+++ hurricaine/src/Components/Searchbar.js
@@ -14,6 +14,7 @@ const Searchbar = () => {
   const [speechStart, setSpeechStart] = useState(false);
   const [placeholder, setPlaceholder] = useState("");
   const searchInput = document.getElementById("search-input");
+  const [mapOpen, setMapOpen] = useState(false);
   const [searchBarClass, setSearchBarClass] = useState("flex justify-center align-middle mt-40");
   const createRequestBody = () => {
     const requestBody = [];
@@ -140,6 +141,10 @@ const Searchbar = () => {
     }
   };
 
+  const showMap = (state) => {
+    setMapOpen(state)
+  }
+
   return (
     <div className="convo-box">
       <div className="overflow-scroll search-result-container" id="Scroller">
@@ -147,22 +152,22 @@ const Searchbar = () => {
           <div className="w-1/2 text-start" id="searchResult" >
           </div>
         </div>
-        {input.includes("where") ? <Map className="w-full" /> : <br />}
+        {mapOpen ? <Map className="w-full" /> : <br />}
       </div>
       <div className={searchBarClass}>
         <div className="w-1/2 h-12 border-2 border-grey-500 rounded flex justify-end">
           {/* defaultValue is needed for speech transcript */}
-          <input class="inputBar" id="search-input" defaultValue={input} placeholder={placeholder} onInput={e => setInput(e.target.value)} onKeyDown={handleKeyPress} />
+          <input className="inputBar" id="search-input" defaultValue={input} placeholder={placeholder} onInput={e => setInput(e.target.value)} onKeyDown={handleKeyPress} />
           {!speechStart ?
-            <div class="innerElement">
+            <div className="innerElement">
               {!browserSupportsSpeechRecognition ?
-                <div class="innerElement">
+                <div className="innerElement">
                   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="grey" className="w-6 h-6">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M12 18.75a6 6 0 006-6v-1.5m-6 7.5a6 6 0 01-6-6v-1.5m6 7.5v3.75m-3.75 0h7.5M12 15.75a3 3 0 01-3-3V4.5a3 3 0 116 0v8.25a3 3 0 01-3 3z" />
                   </svg>
                 </div>
                 :
-                <div class="innerElement" onClick={listen}>
+                <div className="innerElement" onClick={listen}>
                   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M12 18.75a6 6 0 006-6v-1.5m-6 7.5a6 6 0 01-6-6v-1.5m6 7.5v3.75m-3.75 0h7.5M12 15.75a3 3 0 01-3-3V4.5a3 3 0 116 0v8.25a3 3 0 01-3 3z" />
                   </svg>
@@ -170,20 +175,26 @@ const Searchbar = () => {
               }
             </div>
             :
-            <div class="innerElement" onClick={listen}>
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+            <div className="innerElement" onClick={listen}>
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
               </svg>
             </div>
           }
-          <div class="searchElement" onClick={handleSearch}>
+          <div className="searchElement -mr-2" onClick={()=>showMap(!mapOpen)}>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-7">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z" />
+              <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z" />
+            </svg>
+          </div>
+          <div className="searchElement" onClick={handleSearch}>
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
               <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
             </svg>
           </div>
         </div>
       </div>
-      {!browserSupportsSpeechRecognition ? <div>Browser doesn't support speech recognition.</div> : ""}
+      {!browserSupportsSpeechRecognition ? <div className="mt-2">Browser doesn't support speech recognition.</div> : ""}
     </div>
   )
 }
